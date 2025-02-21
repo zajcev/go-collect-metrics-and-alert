eeec08dc66c2bba31d4bdd55cd1a450599de9799
mzaytsev <mzaytsev@inno.tech>
2025-02-22 02:28:05 +0300

    iter8: idk

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/compress"
 	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/config"
 	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/handlers"
+	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/logging"
 	"log"
 	"net/http"
 )
 
 func Router() chi.Router {
 	r := chi.NewRouter()
-	r.Use(compress.GzipMiddleware)
-	//r.Use(logging.ZapMiddleware)
+	//r.Use(compress.GzipMiddleware)
+	r.Use(logging.ZapMiddleware)
 	r.Post("/update/{type}/{name}/{value}", handlers.UpdateMetricHandler)
 	r.Post("/update/", handlers.UpdateMetricHandlerJSON)
 	r.Post("/value/", handlers.GetMetricHandlerJSON)
