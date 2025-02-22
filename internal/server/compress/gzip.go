86352edc36d21c072d42dba52faf7fb30c40e4a3
mzaytsev <mzaytsev@inno.tech>
2025-02-22 13:56:04 +0300

    iter8: test

--- internal/server/compress/gzip.go
+++ internal/server/compress/gzip.go
@@ -28,9 +28,6 @@ func (c *compressWriter) Write(p []byte) (int, error) {
 }
 
 func (c *compressWriter) WriteHeader(statusCode int) {
-	if statusCode < 300 {
-		c.w.Header().Set("Content-Encoding", "gzip")
-	}
 	c.w.WriteHeader(statusCode)
 }
 
