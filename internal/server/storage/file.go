32263e0a9cbd5d5c087e16f5ffab416137297579
mzaytsev <mzaytsev@inno.tech>
2025-02-24 11:19:38 +0300

    iter9  added save/restore metrics file  added new flags/env variable

---
+++ internal/server/storage/file.go
@@ -0,0 +1,62 @@
+package storage
+
+import (
+	"encoding/json"
+	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/models"
+	"os"
+)
+
+type Producer struct {
+	file    *os.File
+	encoder *json.Encoder
+}
+
+func NewProducer(fileName string) (*Producer, error) {
+	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Producer{
+		file:    file,
+		encoder: json.NewEncoder(file),
+	}, nil
+}
+
+func (p *Producer) WriteMetrics(metrics *models.MemStorage) error {
+	return p.encoder.Encode(&metrics)
+}
+
+func (p *Producer) Close() error {
+	return p.file.Close()
+}
+
+type Consumer struct {
+	file    *os.File
+	decoder *json.Decoder
+}
+
+func NewConsumer(fileName string) (*Consumer, error) {
+	file, err := os.OpenFile(fileName, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		file:    file,
+		decoder: json.NewDecoder(file),
+	}, nil
+}
+
+func (c *Consumer) ReadMetrics() (*models.MemStorage, error) {
+	metric := &models.MemStorage{}
+	if err := c.decoder.Decode(&metric); err != nil {
+		return nil, err
+	}
+
+	return metric, nil
+}
+
+func (c *Consumer) Close() error {
+	return c.file.Close()
+}
