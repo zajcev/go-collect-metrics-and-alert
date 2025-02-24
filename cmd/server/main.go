0f6b73514f9cbbfe87137d4fd012e388980a79fb
mzaytsev <mzaytsev@inno.tech>
2025-02-24 23:47:14 +0300

    iter9 : test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/jasonlvhit/gocron"
-	"github.com/zajcev/go-collect-metrics-and-alert/internal/convert"
 	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/compress"
 	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/config"
 	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/handlers"
@@ -31,14 +29,14 @@ func main() {
 		log.Printf("Error: %v\n", err)
 	}
 	env := config.GetFlags()
-	go func() {
-		if env.Restore {
-			handlers.RestoreMetricStorage(env.FilePath)
-		}
-		if env.StoreInterval != 0 {
-			gocron.Every(convert.GetUint(env.StoreInterval)).Second().Do(handlers.SaveMetricStorage, env.FilePath)
-			<-gocron.Start()
-		}
-	}()
+	//go func() {
+	//	if env.Restore {
+	//		handlers.RestoreMetricStorage(env.FilePath)
+	//	}
+	//	if env.StoreInterval != 0 {
+	//		gocron.Every(convert.GetUint(env.StoreInterval)).Second().Do(handlers.SaveMetricStorage, env.FilePath)
+	//		<-gocron.Start()
+	//	}
+	//}()
 	log.Fatal(http.ListenAndServe(env.Address, Router()))
 }
