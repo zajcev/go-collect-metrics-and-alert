5f98dbc321f95d6259e901d8793df48d57a51b20
mzaytsev <mzaytsev@inno.tech>
2025-02-24 11:43:33 +0300

    iter9 : test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/jasonlvhit/gocron"
-	"github.com/zajcev/go-collect-metrics-and-alert/internal/convert"
 	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/compress"
 	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/config"
 	"github.com/zajcev/go-collect-metrics-and-alert/internal/server/handlers"
@@ -30,13 +28,13 @@ func main() {
 	go func() {
 		log.Fatal(http.ListenAndServe(env.Address, Router()))
 	}()
-	if env.Restore {
-		handlers.RestoreMetricStorage(env.FilePath)
-	}
-	err := gocron.Every(convert.GetUint(env.StoreInterval)).Second().Do(handlers.SaveMetricStorage, env.FilePath)
-	if err != nil {
-		return
-	}
-	<-gocron.Start()
+	//if env.Restore {
+	//	handlers.RestoreMetricStorage(env.FilePath)
+	//}
+	//err := gocron.Every(convert.GetUint(env.StoreInterval)).Second().Do(handlers.SaveMetricStorage, env.FilePath)
+	//if err != nil {
+	//	return
+	//}
+	//<-gocron.Start()
 
 }
